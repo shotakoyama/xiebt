b86413cb929790c8b6e190e10b8b27a6df4b8434
shotakoyama <koyamashota@gmail.com>
2023-02-09 14:58:22 +0900

    for 0.12

--- xiebt/generate.py
+++ xiebt/generate.py
@@ -1,32 +1,34 @@
 import torch
 from fairseq import options, utils
+from fairseq.dataclass.utils import convert_namespace_to_omegaconf
 from .task import BackTranslationTask
 from .util import *
 
-def xiebt_generate(args):
+def xiebt_generate(cfg):
     logger = make_logger()
-    logger.info(args)
-    fix_seed(args)
-    use_cuda = torch.cuda.is_available() and not args.cpu
-    task = BackTranslationTask.setup_task(args)
-    task.load_dataset(args.gen_subset)
+    logger.info(cfg)
+    fix_seed(cfg)
+    use_cuda = torch.cuda.is_available() and not cfg.common.cpu
+    task = BackTranslationTask.setup_task(cfg.task)
+    task.load_dataset(cfg.dataset.gen_subset)
     src_dict, tgt_dict = load_dicts(task)
-    models = load_models(logger, args, task, use_cuda)
-    progress = make_progress_bar(args, task, models)
-    generator = task.build_generator(models, args)
+    models = load_models(logger, cfg, task, use_cuda)
+    progress = make_progress_bar(cfg, task, models)
+    generator = task.build_generator(models, cfg)
 
     for sample in progress:
         sample = utils.move_to_cuda(sample) if use_cuda else sample
         hypos = task.inference_step(generator, models, sample)
 
         for i, sample_id in enumerate(sample["id"].tolist()):
-            print_hypos(args, sample, hypos, generator, i, sample_id, src_dict, tgt_dict)
+            print_hypos(cfg, sample, hypos, generator, i, sample_id, src_dict, tgt_dict)
 
 
 def main():
     parser = options.get_generation_parser()
     parser.add_argument('--beta-random', default = 8.0, type = float)
     args = options.parse_args_and_arch(parser)
-    xiebt_generate(args)
+    cfg = convert_namespace_to_omegaconf(args)
+    xiebt_generate(cfg)
 
 
--- xiebt/util.py
+++ xiebt/util.py
@@ -16,10 +16,10 @@ def make_logger():
     return logger
 
 
-def fix_seed(args):
-    if args.seed is not None and not args.no_seed_provided:
-        np.random.seed(args.seed)
-        utils.set_torch_seed(args.seed)
+def fix_seed(cfg):
+    if cfg.common.seed is not None and not cfg.generation.no_seed_provided:
+        np.random.seed(cfg.common.seed)
+        utils.set_torch_seed(cfg.common.seed)
 
 
 def load_dicts(task):
@@ -31,63 +31,62 @@ def load_dicts(task):
     return src_dict, tgt_dict
 
 
-def load_models(logger, args, task, use_cuda):
-    logger.info("loading model(s) from {}".format(args.path))
-    models, _model_args = checkpoint_utils.load_model_ensemble(
-        utils.split_paths(args.path),
-        task=task,
-        suffix=getattr(args, "checkpoint_suffix", ""),
-        strict=(args.checkpoint_shard_count == 1),
-        num_shards=args.checkpoint_shard_count)
+def load_models(logger, cfg, task, use_cuda):
+    logger.info("loading model(s) from {}".format(cfg.common_eval.path))
+    models, save_cfg = checkpoint_utils.load_model_ensemble(
+        utils.split_paths(cfg.common_eval.path),
+        task = task,
+        suffix = cfg.checkpoint.checkpoint_suffix,
+        strict = (cfg.checkpoint.checkpoint_shard_count == 1),
+        num_shards = cfg.checkpoint.checkpoint_shard_count)
 
     for model in models:
         if model is None:
             continue
-        if args.fp16:
+        if cfg.common.fp16:
             model.half()
-        if use_cuda and not args.pipeline_model_parallel:
+        if use_cuda and not cfg.distributed_training.pipeline_model_parallel:
             model.cuda()
-        model.prepare_for_inference_(args)
+        model.prepare_for_inference_(cfg)
     return models
 
 
-def make_progress_bar(args, task, models):
+def make_progress_bar(cfg, task, models):
     itr = task.get_batch_iterator(
-        dataset=task.dataset(args.gen_subset),
-        max_tokens=args.max_tokens,
-        max_sentences=args.batch_size,
-        max_positions=utils.resolve_max_positions(
-            task.max_positions(), *[model.max_positions() for model in models]
-        ),
-        ignore_invalid_inputs=args.skip_invalid_size_inputs_valid_test,
-        required_batch_size_multiple=args.required_batch_size_multiple,
-        num_shards=args.num_shards,
-        shard_id=args.shard_id,
-        num_workers=args.num_workers,
-        data_buffer_size=args.data_buffer_size,
-    ).next_epoch_itr(shuffle=False)
+        dataset = task.dataset(cfg.dataset.gen_subset),
+        max_tokens = cfg.dataset.max_tokens,
+        max_sentences = cfg.dataset.batch_size,
+        max_positions = utils.resolve_max_positions(
+            task.max_positions(), *[model.max_positions() for model in models]),
+        ignore_invalid_inputs = cfg.dataset.skip_invalid_size_inputs_valid_test,
+        required_batch_size_multiple = cfg.dataset.required_batch_size_multiple,
+        num_shards = cfg.distributed_training.distributed_world_size,
+        shard_id = cfg.distributed_training.distributed_rank,
+        num_workers = cfg.dataset.num_workers,
+        data_buffer_size = cfg.dataset.data_buffer_size,
+    ).next_epoch_itr(shuffle = False)
     progress = progress_bar.progress_bar(
         itr,
-        log_format=args.log_format,
-        log_interval=args.log_interval,
-        default_log_format=("tqdm" if not args.no_progress_bar else "none"))
+        log_format = cfg.common.log_format,
+        log_interval = cfg.common.log_interval,
+        default_log_format = ("tqdm" if not cfg.common.no_progress_bar else "simple"))
     return progress
 
 
-def print_hypos(args, sample, hypos, generator, i, sample_id, src_dict, tgt_dict):
+def print_hypos(cfg, sample, hypos, generator, i, sample_id, src_dict, tgt_dict):
     src_tokens = utils.strip_pad(sample["net_input"]["src_tokens"][i, :], tgt_dict.pad())
-    src_str = src_dict.string(src_tokens, args.remove_bpe)
+    src_str = src_dict.string(src_tokens, cfg.common_eval.post_process)
     print("S-{}\t{}".format(sample_id, src_str))
 
     # Process top predictions
-    for j, hypo in enumerate(hypos[i][: args.nbest]):
+    for j, hypo in enumerate(hypos[i][: cfg.generation.nbest]):
         hypo_tokens, hypo_str, alignment = utils.post_process_prediction(
-            hypo_tokens=hypo["tokens"].int().cpu(),
-            src_str=src_str,
-            alignment=hypo["alignment"],
-            align_dict=None,
-            tgt_dict=tgt_dict,
-            remove_bpe=args.remove_bpe,
+            hypo_tokens = hypo["tokens"].int().cpu(),
+            src_str = src_str,
+            alignment = hypo["alignment"],
+            align_dict = None,
+            tgt_dict = tgt_dict,
+            remove_bpe = cfg.common_eval.post_process,
             extra_symbols_to_ignore = {generator.eos})
         score = hypo["score"] / math.log(2)  # convert to base 2
         # original hypothesis (after tokenization and BPE)
